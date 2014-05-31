5b4a4a5feda36fe1d7984d9b9886ca0330188598
Andre C Meireles <andre.meireles@knights.ucf.edu>
2014-05-31 16:51:11 -0400

    non unique elements

---
+++ checkio_non_unique_elements.py
@@ -0,0 +1,33 @@
+# Andre Meireles
+# Problem submission for check.io
+# Non-unique Elements
+# http://www.checkio.org/mission/non-unique-elements/solve/
+
+
+#Your optional code here
+#You can import some modules or create additional functions
+
+
+def checkio(data):
+
+    result = []
+
+    for element in data:
+        if data.count(element) > 1:
+            result.append(element)
+
+    return result
+
+#Some hints
+#You can use list.count(element) method for counting.
+#Create new list with non-unique elements
+#or remove elements from original list (but it's bad practice for many real cases)
+#Loop over original list
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
